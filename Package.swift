00697f5d11f769390fdd814d348bd38f7dc28b9b
Nikita Konopelko <nik.sativa@gmail.com>
2021-11-07 21:09:35 +0200

    spm only proj

--- Package.swift
+++ Package.swift
@@ -2,23 +2,32 @@
 
 import PackageDescription
 
-let package = Package(name: "NSpry",
-                      platforms: [.iOS(.v10)],
-                      products: [.library(name: "NSpry", targets: ["NSpry"]),
-                                 .library(name: "NSpry_Nimble", targets: ["NSpry_Nimble"])],
-                      dependencies: [.package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "4.0.0")),
-                                     .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.2.0"))],
-                      targets: [.target(name: "NSpry",
-                                        dependencies: [],
-                                        path: "Source/Core"),
-                                .target(name: "NSpry_Nimble",
-                                        dependencies: ["NSpry",
-                                                       "Nimble"],
-                                        path: "Source/Nimble"),
-                                .testTarget(name: "NSpryTests",
-                                            dependencies: ["NSpry",
-                                                           "NSpry_Nimble",
-                                                           "Nimble",
-                                                           "Quick"],
-                                            path: "Tests")],
-                      swiftLanguageVersions: [.v5])
+// swiftformat:disable all
+let package = Package(
+    name: "NSpry",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "NSpry", targets: ["NSpry"]),
+        .library(name: "NSpry_Nimble", targets: ["NSpry_Nimble"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.2.0"))
+    ],
+    targets: [
+        .target(name: "NSpry",
+                dependencies: [],
+                path: "Source/Core"),
+        .target(name: "NSpry_Nimble",
+                dependencies: ["NSpry",
+                               "Nimble"],
+                path: "Source/Nimble"),
+        .testTarget(name: "NSpryTests",
+                    dependencies: ["NSpry",
+                                   "NSpry_Nimble",
+                                   "Nimble",
+                                   "Quick"],
+                    path: "Tests")
+    ],
+    swiftLanguageVersions: [.v5]
+)
