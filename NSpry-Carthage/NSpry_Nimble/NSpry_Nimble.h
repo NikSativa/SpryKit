427b24681c936d4f35eafc2004578ea091978c81
Max Kazakov <kazakov.maixm.a@gmail.com>
2021-11-15 16:03:38 +0500

    carthage support

---
+++ NSpry-Carthage/NSpry_Nimble/NSpry_Nimble.h
@@ -0,0 +1,18 @@
+//
+//  NSpry_Nimble.h
+//  NSpry_Nimble
+//
+//  Created by Максим Казаков on 15.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NSpry_Nimble.
+FOUNDATION_EXPORT double NSpry_NimbleVersionNumber;
+
+//! Project version string for NSpry_Nimble.
+FOUNDATION_EXPORT const unsigned char NSpry_NimbleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSpry_Nimble/PublicHeader.h>
+
+
