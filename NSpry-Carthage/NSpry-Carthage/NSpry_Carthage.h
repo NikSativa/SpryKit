9ecd79110c88e7f4979a7ad1ce2e44b55ff15a84
Nikita Konopelko <nik.sativa@gmail.com>
2021-11-17 22:19:00 +0200

    revert carthage project

---
+++ NSpry-Carthage/NSpry-Carthage/NSpry_Carthage.h
@@ -0,0 +1,18 @@
+//
+//  NSpry_Carthage.h
+//  NSpry-Carthage
+//
+//  Created by Максим Казаков on 15.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NSpry_Carthage.
+FOUNDATION_EXPORT double NSpry_CarthageVersionNumber;
+
+//! Project version string for NSpry_Carthage.
+FOUNDATION_EXPORT const unsigned char NSpry_CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSpry_Carthage/PublicHeader.h>
+
+
