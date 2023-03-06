9dc2621796a904c21e1d91c53f798b2e6dc5fb2a
Nikita Konopelko <nik.sativa@gmail.com>
2023-03-06 12:08:09 +0200

    lil update UTs

--- Tests/SpryableSpec.swift
+++ Tests/SpryableSpec.swift
@@ -15,29 +15,48 @@ final class SpryableSpec: QuickSpec {
                 SpryableTestClass.resetCallsAndStubs()
             }
 
-            describe("recording calls - instance") {
-                beforeEach {
-                    subject.stub(.getAString).andReturn("")
+            describe("recording calls - instance; with arguments") {
+                var actual: String!
 
-                    _ = subject.getAString(string: "")
+                beforeEach {
+                    subject.stub(.getAStringWithArguments).andReturn("out")
+                    actual = subject.getAString(string: "in")
+                }
+
+                it("should have recorded the call using Spyable") {
+                    let result = subject.didCall(.getAStringWithArguments)
+                    expect(result.success).to(beTrue())
+                    expect(actual) == "out"
+                }
+            }
+
+            describe("recording calls - instance") {
+                var actual: String!
+
+                beforeEach {
+                    subject.stub(.getAString).andReturn("out")
+                    actual = subject.getAString()
                 }
 
                 it("should have recorded the call using Spyable") {
                     let result = subject.didCall(.getAString)
                     expect(result.success).to(beTrue())
+                    expect(actual) == "out"
                 }
             }
 
             describe("recording calls - class") {
-                beforeEach {
-                    SpryableTestClass.stub(.getAStaticString).andReturn("")
+                var actual: String!
 
-                    _ = SpryableTestClass.getAStaticString()
+                beforeEach {
+                    SpryableTestClass.stub(.getAStaticString).andReturn("out")
+                    actual = SpryableTestClass.getAStaticString()
                 }
 
                 it("should have recorded the call using Spyable") {
                     let result = SpryableTestClass.didCall(.getAStaticString)
                     expect(result.success).to(beTrue())
+                    expect(actual) == "out"
                 }
             }
 
@@ -49,7 +68,7 @@ final class SpryableSpec: QuickSpec {
                 }
 
                 it("should return the stubbed value using Stubbable") {
-                    expect(subject.getAString(string: "")).to(equal(expectedString))
+                    expect(subject.getAString()).to(equal(expectedString))
                 }
             }
 
@@ -68,14 +87,14 @@ final class SpryableSpec: QuickSpec {
             describe("reseting calls and stubs - instance") {
                 beforeEach {
                     subject.stub(.getAString).andReturn("")
-                    _ = subject.getAString(string: "")
+                    _ = subject.getAString()
 
                     subject.resetCallsAndStubs()
                 }
 
                 it("should reset the calls and the stubs") {
                     expect(subject.didCall(.getAString).success).to(beFalse())
-                    expect({ _ = subject.getAString(string: "") }()).to(throwAssertion())
+                    expect({ _ = subject.getAString() }()).to(throwAssertion())
                 }
             }
 
