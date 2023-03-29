5b2e41476fc74ca9c627a43d9ed76f77ea9ad459
Nikita Konopelko <nik.sativa@gmail.com>
2023-03-29 17:29:49 +0300

    HaveRecordedCallsMatcherTests

---
+++ Tests/HaveRecordedCallsMatcherTests.swift
@@ -0,0 +1,24 @@
+import Foundation
+import NSpry
+import XCTest
+
+final class HaveRecordedCallsMatcherTests: XCTestCase {
+    func test_at_least_one_call_has_been_made() {
+        let subject: SpyableTestHelper = .init()
+
+        XCTAssertHaveNoRecordedCalls(subject)
+        XCTAssertHaveNoRecordedCalls(SpyableTestHelper.self)
+
+        subject.doStuff()
+        XCTAssertHaveRecordedCalls(subject)
+
+        SpyableTestHelper.doClassStuff()
+        XCTAssertHaveRecordedCalls(SpyableTestHelper.self)
+
+        subject.resetCalls()
+        XCTAssertHaveNoRecordedCalls(subject)
+
+        SpyableTestHelper.resetCalls()
+        XCTAssertHaveNoRecordedCalls(SpyableTestHelper.self)
+    }
+}
