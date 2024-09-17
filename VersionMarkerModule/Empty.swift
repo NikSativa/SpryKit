f095029653f7ed9c026db486a362f72c774fd6bc
Mykyta Konopelko <nik.sativa@gmail.com>
2024-09-17 10:38:51 +0300

    added SpryMacroAvailable

---
+++ VersionMarkerModule/Empty.swift
@@ -0,0 +1,3 @@
+// The SpryMacroAvailable module is intentionally empty.
+// It serves as an indicator of whether @Spryable-macro is available.
+// See the 'README -> #MacroAvailable' document for more information.
