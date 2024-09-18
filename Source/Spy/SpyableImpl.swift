66562081ead0424035a5c5aac0b52151b2d36f02
Mykyta Konopelko <nik.sativa@gmail.com>
2024-09-18 14:09:36 +0300

    fix Swift 5.9 features

--- Source/Spy/SpyableImpl.swift
+++ Source/Spy/SpyableImpl.swift
@@ -3,7 +3,7 @@ import Foundation
 /// A global NSMapTable to hold onto calls for types conforming to Spyable. This map table has "weak to strong objects" options.
 ///
 /// - Important: Do NOT use this object.
-#if swift(>=5.9)
+#if swift(>=6.0)
 private nonisolated(unsafe) var callsMapTable: NSMapTable<AnyObject, SpryDictionary<RecordedCall>> = NSMapTable.weakToStrongObjects()
 #else
 private var callsMapTable: NSMapTable<AnyObject, SpryDictionary<RecordedCall>> = NSMapTable.weakToStrongObjects()
